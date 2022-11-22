ec767665c5aea9f79ba43985ed9f7a7b252de68d
VCyril <ver17hovsk@gmail.com>
2022-11-23 00:11:05 +0300

    added env config

--- go.mod
+++ go.mod
@@ -3,7 +3,10 @@ module github.com/kaverhovsky/gosniias-time-manager-bot
 go 1.18
 
 require (
+	github.com/asaskevich/govalidator v0.0.0-20210307081110-f21760c49a8d // indirect
 	github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1 // indirect
+	github.com/joho/godotenv v1.4.0 // indirect
+	github.com/kelseyhightower/envconfig v1.4.0 // indirect
 	go.uber.org/atomic v1.10.0 // indirect
 	go.uber.org/multierr v1.8.0 // indirect
 	go.uber.org/zap v1.23.0 // indirect
--- pkg/common/config.go
+++ pkg/common/config.go
@@ -1,1 +1,65 @@
 package common
+
+import (
+	"github.com/asaskevich/govalidator"
+	"github.com/joho/godotenv"
+	"github.com/kelseyhightower/envconfig"
+	"go.uber.org/zap"
+)
+
+// Config - struct that stores configuration of the app
+type Config struct {
+	Debug       bool   `envconfig:"DEBUG" default:"false" valid:"optional,type(bool)"`
+	Development bool   `envconfig:"DEVELOPMENT" default:"false" valid:"optional,type(bool)"`
+	LogLevel    string `envconfig:"LOGLEVEL" default:"info" valid:"in(debug|info|warning|error)"`
+	BotToken    string `envconfig:"BOT_TOKEN" valid:"required,string"`
+}
+
+// ReadConfig returns *Config with app configuration
+func ReadConfig(path string, log *zap.Logger) (*Config, error) {
+	logger := log.Named("config")
+	if err := loadConfig(path); err != nil {
+		return nil, err
+	}
+
+	var cfg Config
+	if err := envconfig.Process("", &cfg); err != nil {
+		return nil, err
+	}
+
+	if _, err := govalidator.ValidateStruct(cfg); err != nil {
+		return nil, err
+	}
+
+	logger.With(zap.Any("config", cfg)).Info("Config loaded")
+	return &cfg, nil
+}
+
+func loadConfig(path string) error {
+	if path == "" {
+		return nil
+	}
+	if err := godotenv.Load(path); err != nil {
+		return err
+	}
+	return nil
+}
+
+// Mode returns application mode: "production" or "development".
+func (c Config) Mode() string {
+	if c.Development {
+		return "development"
+	}
+	return "production"
+}
+
+// GetLogLevel returns logging level with respect to application mode.
+func (c Config) Level() string {
+	if c.Debug {
+		return "debug"
+	}
+	if c.LogLevel == "warning" {
+		return "warn"
+	}
+	return c.LogLevel
+}
