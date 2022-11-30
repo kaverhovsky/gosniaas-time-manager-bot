8470dfabc868845da42d9538e9ef561f2af34267
kaverhovsky <ver17hovsk@gmail.com>
2022-11-30 19:18:14 +0300

    added scheduler initialisation from csv file of durations

--- internal/usecase/scheduler/scheduler.go
+++ internal/usecase/scheduler/scheduler.go
@@ -1,17 +1,80 @@
 package scheduler
 
+import (
+	"encoding/csv"
+	"errors"
+	"github.com/kaverhovsky/gosniias-time-manager-bot/internal/domain"
+	"go.uber.org/zap"
+	"os"
+	"time"
+)
+
+const (
+	durationsFilename = "durations.csv"
+	layout            = ""
+)
+
 type scheduler struct {
+	durations map[int64]time.Duration
+	logger    *zap.Logger
 }
 
 var sch *scheduler
 
 func Get() Scheduler {
 	if sch == nil {
-		sch = create()
+		return nil
 	}
 	return sch
 }
 
-func create() *scheduler {
-	return &scheduler{}
+func New(logger *zap.Logger) *scheduler {
+	return &scheduler{
+		durations: make(map[int64]time.Duration),
+		logger:    logger,
+	}
+}
+
+func (sch *scheduler) init() error {
+	// TODO добавить логи
+	f, err := os.Open(durationsFilename)
+	if err != nil {
+		return err
+	}
+	r := csv.NewReader(f)
+	rows, err := r.ReadAll()
+	if err != nil {
+		return err
+	}
+	for _, row := range rows {
+		if len(row) != 2 {
+			return errors.New("wrong row format in \"durations\" file")
+		}
+
+		datetime, err := time.Parse(layout, row[0])
+		if err != nil {
+			// TODO обернуть ошибку
+			return err
+		}
+
+		// add record to scheduler's durations
+		dur, err := time.ParseDuration(row[1])
+		if err != nil {
+			// TODO обернуть ошибку
+			return err
+		}
+		sch.durations[datetime.Unix()] = dur
+	}
+
+	return nil
+}
+
+func (sch *scheduler) GetPeriod(now time.Time) *domain.Period {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (sch *scheduler) SumForNow(now time.Time) time.Duration {
+	//TODO implement me
+	panic("implement me")
 }
